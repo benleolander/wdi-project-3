762ae9f5ee69c78edcbade393d24d156efb59fba
Ben Lander <ben.l.lander@googlemail.com>
2019-02-08 11:13:47 +0000

    Added Creator prevalidate password check match function

--- models/creator.js
+++ models/creator.js
@@ -8,4 +8,19 @@ const creatorSchema = new mongoose.Schema({
   items: { type: Array } //array of references
 })
 
+creatorSchema.virtual('passwordConfirmation')
+  .set(function setPasswordConfirmation(passwordConfirmation) {
+    this._passwordConfirmation = passwordConfirmation
+  })
+
+creatorSchema.pre('validate', function checkPasswordsMath(next) {
+  if(
+    this.isModified('password') &&
+    this._passwordConfirmation !== this.password
+  ) {
+    this.invalidate('passwordConfirmation', 'Passwords do not match')
+  }
+  next()
+})
+
 module.exports = mongoose.model('Creator', creatorSchema)

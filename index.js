36eb3e0f54d83524af8ded8a469a4513726d5b01
Ben Lander <ben.l.lander@googlemail.com>
2019-02-08 10:09:50 +0000

    Added Express Server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+mongoose.connect = ('mongodb://localhost/homemade')
+
+app.use(bodyParser.json())
+
+app.listen(4000, () => console.log('Express is running on Port 4000'))

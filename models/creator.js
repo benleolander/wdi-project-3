b30be59d63d6d4b7ce79cfeef1e8bd883a5e496b
SirAbbott <tom@iynk.com>
2019-02-08 14:31:50 +0000

    commiting for Beth to pull newest code

--- models/creator.js
+++ models/creator.js
@@ -6,7 +6,7 @@ const creatorSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: 'Email taken, please choose another'},
   password: { type: String, required: 'Password required'},
   image: { type: String },
-  items: { type: Array } //array of references
+  items: { type: Array } 
 })
 
 creatorSchema.virtual('passwordConfirmation')

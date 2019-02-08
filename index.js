02d466bb7be69fa817e86727041b9ecfcc52b3e8
SirAbbott <tom@iynk.com>
2019-02-08 13:35:14 +0000

    error handling set up in lib folder

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+mongoose.plugin(require('mongoose-unique-validator'))
 const bodyParser = require('body-parser')
 const routes = require('./config/routes')
 

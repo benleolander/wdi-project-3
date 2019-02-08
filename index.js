ac08c9fcfa1d1f4d84b94fe68ed7aab0df036f5a
SirAbbott <tom@iynk.com>
2019-02-08 13:15:36 +0000

    edit to routes

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const routes = require('./config/routes')
 
 const app = express()
 
 mongoose.connect = ('mongodb://localhost/homemade')
 
 app.use(bodyParser.json())
+
+app.use('/api', routes)
 
 app.listen(4000, () => console.log('Express is running on Port 4000'))

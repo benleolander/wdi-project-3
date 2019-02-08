21ae94300a82dd5e5bd4116b800e380b7056e21e
Ben Lander <ben.l.lander@googlemail.com>
2019-02-08 11:03:17 +0000

    Turned creator model into Schema

--- models/creator.js
+++ models/creator.js
@@ -1,7 +1,11 @@
-const Creator = {
+const mongoose = require('mongoose')
+
+const creatorSchema = new mongoose.Schema({
   username: { type: String, required: true},
   email: { type: String, required: true},
   password: { type: String, required: true},
   image: { type: String },
   items: { type: Array } //array of references
-}
+})
+
+module.exports = mongoose.model('Creator', creatorSchema)

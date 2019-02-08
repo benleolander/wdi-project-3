9c9988d7b728c36eacffd48717ae08e6645bbd19
Dexter De Leon <dexdeleon@gmail.com>
2019-02-08 11:18:04 +0000

    Navbar component added with bulma. React Router DOM added.

--- src/app.js
+++ src/app.js
@@ -1,17 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
+import 'bulma'
 import './style.scss'
+
+import Navbar from './components/common/Navbar'
+
+const data = [
+  {
+    _id: 2,
+    name: 'Elven table',
+    image: 'http://www.spencerfieldlarcombe.com/images/_cached/400/1809434.jpg',
+    creator: 'John Doe',
+    description: 'Table for any Gandalf out in the West',
+    categories: ['Carpentry', 'Tables']
+  }
+]
 
 class App extends React.Component {
   constructor(){
     super()
   }
 
+  componentDidMount(){
+    this.setState({ data })
+  }
 
   render(){
     return(
-      <h1>Sup</h1>
+      <div>
+        <BrowserRouter>
+          <div>
+            <Navbar />
+
+            <Switch>
+              <Route path="/" />
+            </Switch>
+
+          </div>
+        </BrowserRouter>
+      </div>
     )
   }
 }

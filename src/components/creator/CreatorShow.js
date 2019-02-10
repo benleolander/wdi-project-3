2e6e45570b6d18b13abf23bd0c18b69252057b07
SirAbbott <tom@iynk.com>
2019-02-10 12:11:57 +0000

    changes to profile pic

--- src/components/creator/CreatorShow.js
+++ src/components/creator/CreatorShow.js
@@ -18,11 +18,20 @@ class CreatorShow extends React.Component{
   render(){
     if (!this.state.creator) return <p>Loading...</p>
     console.log(this.state.creator)
-    const { username } = this.state.creator
+    const { username, image } = this.state.creator
     return(
-      <div>
-        <h1 className="title is-3">{username}</h1>
-      </div>
+      <section className="section">
+        <div className="profilePicDiv container">
+          <div
+            className="image is-square"
+            style={{
+              backgroundImage: `url(${image})`
+            }}
+          >
+          </div>
+          <h1 className="title is-3">{username}</h1>
+        </div>
+      </section>
     )
   }
 }

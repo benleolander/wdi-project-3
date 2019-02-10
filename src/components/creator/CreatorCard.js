391859d4a87da8e632ce8f35def6fcb9363b9c42
Dexter De Leon <dexdeleon@gmail.com>
2019-02-10 08:59:16 +0000

    Creator Card linking to Creator Show page now on the Item Show page. Height transition added to navbar burger menu activation.

--- src/components/creator/CreatorCard.js
+++ src/components/creator/CreatorCard.js
@@ -1,23 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const CreatorCard = ({ creator }) => {
   return (
-    <div className="card">
-      <header className="card-header">
-        <h3 className="card-header-title">More about {creator.username}</h3>
-      </header>
-      <div className="card-image">
-        <figure className="image is-square">
-          <img src={creator.image} alt={`${creator.username}'s Profile Image`} />
-        </figure>
-      </div>
-      <div className="card-content">
-        <div className="content">
-          Creator bio, optional? Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Phasellus nec iaculis mauris.
+    <Link to={`/creators/${creator._id}`}>
+      <div className="card creatorProfile">
+        <header className="card-header">
+          <h3 className="card-header-title">More about {creator.username}</h3>
+        </header>
+        <div className="card-image">
+          <figure className="image is-square">
+            <img src={creator.image} alt={`${creator.username}'s Profile Image`} />
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="content">
+            Creator bio, optional? Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Phasellus nec iaculis mauris.
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
 

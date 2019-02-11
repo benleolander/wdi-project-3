71f42e4f6e473e750b72684feb04bcc552aebe12
Ben Lander <ben.l.lander@googlemail.com>
2019-02-11 10:18:13 +0000

    Fixing deployment bugs

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express()
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
 
-// app.use(express.static(`${__dirname}/dist`))
+app.use(express.static(`${__dirname}/dist`))
 app.use(bodyParser.json())
 app.use('/api', routes)
 app.use(errorHandling)

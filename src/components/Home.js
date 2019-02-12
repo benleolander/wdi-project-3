c636ecb1ff6a8c293691ec901f61f979a4561cbb
Dexter De Leon <dexdeleon@gmail.com>
2019-02-12 10:28:04 +0000

    Search bar component rendering on Index, Items Index images made into separate component.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import ItemsIndex from './items/ItemsIndex'
+import SearchBar from './common/SearchBar'
 
 const Home = () => {
   return(
     <section className="section">
       <div className="container">
+        <SearchBar />
         <ItemsIndex />
       </div>
     </section>
--- src/components/items/ItemsIndex.js
+++ src/components/items/ItemsIndex.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios'
-import { Link } from 'react-router-dom'
+
+import ItemImage from './ItemImage'
 
 class ItemsIndex extends React.Component {
 
@@ -15,21 +16,7 @@ class ItemsIndex extends React.Component {
         {
           !this.state ||
           this.state.data.map(item =>
-            <Link
-              to={`/items/${item._id}`}
-              key={item._id}
-              className="itemDiv column is-one-fifth-desktop is-one-quarter-tablet is-one-third-mobile"
-            >
-              <div
-                className="image is-square"
-                style={ {backgroundImage: `url(${item.image})`} }
-              >
-                <div className="itemDescription is-square">
-                  <h3 className="title is-4">{item.name}</h3>
-                  <h3 className="subtitle is-5">by {item.creator.username}</h3>
-                </div>
-              </div>
-            </Link>
+            <ItemImage key={item._id} item={item} />
           )
         }
       </div>

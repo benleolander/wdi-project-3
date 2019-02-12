9388ea3af3cd2fe41cc80ecfbc0f60ad2ea47728
Dexter De Leon <dexdeleon@gmail.com>
2019-02-12 11:36:08 +0000

    Search Bar on index page filtering results by item or creator name.

--- src/components/common/SearchBar.js
+++ src/components/common/SearchBar.js
@@ -1,35 +1,20 @@
 import React from 'react'
 
-class SearchBar extends React.Component {
-  constructor(){
-    super()
-
-    this.state = {
-      search: ''
-    }
-
-    this.handleChange = this.handleChange.bind(this)
-  }
-
-  handleChange({ target: { value, name }}){
-    this.setState({ [name]: value })
-  }
-
-  render(){
-    return(
-      <div className="field">
-        <div className="control">
-          <input
-            className="input"
-            name="search"
-            placeholder="Search..."
-            value={this.state.search}
-            onChange={this.handleChange}
-          />
-        </div>
+const SearchBar = ({ search, handleChange }) => {
+  return(
+    <div className="field">
+      <div className="control">
+        <input
+          className="input"
+          type="text"
+          name="search"
+          placeholder="Search..."
+          value={search}
+          onChange={handleChange}
+        />
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default SearchBar
--- src/components/items/ItemsIndex.js
+++ src/components/items/ItemsIndex.js
@@ -5,20 +5,51 @@ import ItemImage from './ItemImage'
 import SearchBar from '../common/SearchBar'
 
 class ItemsIndex extends React.Component {
+  constructor(){
+    super()
+
+    this.state = {
+      search: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
 
   componentDidMount(){
     axios.get('/api')
       .then(res => this.setState({ data: res.data }))
   }
 
+  handleChange({ target: { value, name }}){
+    this.setState({ [name]: value })
+  }
+
+  filterResults(){
+    if(this.state.search === '') return this.state.data
+    return this.state.data.filter(item =>
+      item.name
+        .toLowerCase()
+        .replace(/ /g , '')
+        .includes(this.state.search) ||
+      item.creator.username
+        .toLowerCase()
+        .replace(/ /g, '')
+        .includes(this.state.search)
+    )
+  }
+
   render(){
     return (
       <div>
-        <SearchBar />
+        <SearchBar
+          handleChange={this.handleChange}
+          search={this.state.search}
+        />
         <div className="indexDiv columns is-gapless is-mobile is-multiline">
           {
-            !this.state ||
-            this.state.data.map(item =>
+            !this.state.data ||
+            this.filterResults().map(item =>
               <ItemImage key={item._id} item={item} />
             )
           }

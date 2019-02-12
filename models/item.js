eac0081a294453d96e235349ef0df22ca2a7baf8
Ben Lander <ben.l.lander@googlemail.com>
2019-02-12 11:20:24 +0000

    Added commentSchema to item model

--- models/item.js
+++ models/item.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose')
 
+const commentSchema = new mongoose.Schema({
+  name: { type: String, required: 'Please enter a display name' },
+  body: { type: String, required: 'Please enter a comment'}
+}, {
+  timestamps: true
+})
+
 const itemSchema = new mongoose.Schema({
   name: { type: String, required: 'Please add a title' },
   image: { type: String, required: 'Please add an image' },
   creator: { type: mongoose.Schema.ObjectId, ref: 'Creator', required: true },
   description: { type: String, required: 'Please add a description' },
-  categories: { type: Array, required: 'Please select at least one catagory' }
+  categories: { type: Array, required: 'Please select at least one catagory' },
+  comments: [ commentSchema ]
 })
 
 

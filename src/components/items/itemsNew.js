76a03e30a4baf236726e37cdc76bf81e2986df59
Dexter De Leon <dexdeleon@gmail.com>
2019-02-12 15:02:35 +0000

    React Select installed and used in add item form.

--- src/components/items/itemsNew.js
+++ src/components/items/itemsNew.js
@@ -1,7 +1,50 @@
 import React from 'react'
+import Select from 'react-select'
+import makeAnimated from 'react-select/lib/animated'
 import axios from 'axios'
 
 import Auth from '../../lib/Auth'
+
+const categories = [
+  { value: 'wood', label: 'Wood' },
+  { value: 'metal', label: 'Metal' },
+  { value: 'creative', label: 'Creative' },
+  { value: 'stairs', label: 'Stairs' },
+  { value: 'chair', label: 'Chair' },
+  { value: 'wicker', label: 'Wicker' },
+  { value: 'lights', label: 'Lights' },
+  { value: 'hanging', label: 'Hanging' },
+  { value: 'kitchen', label: 'Kitchen' },
+  { value: 'bedroom', label: 'Bedroom' },
+  { value: 'rail', label: 'Rail' },
+  { value: 'mounted', label: 'Mounted' },
+  { value: 'bathroom', label: 'Bathroom' },
+  { value: 'drawers', label: 'Drawers' },
+  { value: 'storage', label: 'Storage' },
+  { value: 'homemade', label: 'Homemade' },
+  { value: 'stand', label: 'Stand' },
+  { value: 'living', label: 'Living' },
+  { value: 'steel', label: 'Steel' },
+  { value: 'coats', label: 'Coats' },
+  { value: 'art', label: 'Art' },
+  { value: 'lamp', label: 'Lamp' },
+  { value: 'relax', label: 'Relax' },
+  { value: 'outdoors', label: 'Outdoors' },
+  { value: 'flowers', label: 'Flowers' },
+  { value: 'clock', label: 'Clock' },
+  { value: 'bed', label: 'Bed' },
+  { value: 'boxes', label: 'Boxes' },
+  { value: 'chic', label: 'Chic' },
+  { value: 'indoor', label: 'Indoor' },
+  { value: 'lighting', label: 'Lighting' },
+  { value: 'study', label: 'Study' },
+  { value: 'sofa', label: 'Sofa' },
+  { value: 'wooden', label: 'Wooden' },
+  { value: 'mirror', label: 'Mirror' },
+  { value: 'shelves', label: 'Shelves' },
+  { value: 'outdoors', label: 'Outdoors' }
+]
+
 
 class ItemsNew extends React.Component {
   constructor() {
@@ -75,12 +118,14 @@ class ItemsNew extends React.Component {
             </div>
             <div className="field">
               <label className="label">Categories</label>
-              <input
-                className="input"
-                name="categories"
-                placeholder="Categories"
-                value={this.state.categories}
-                onChange={this.handleChange}
+              <Select
+                isMulti
+                clearValue
+                name="colors"
+                options={categories}
+                components={makeAnimated()}
+                className="basic-multi-select"
+                classNamePrefix="select"
               />
             </div>
             <button className="button is-primary">Submit</button>

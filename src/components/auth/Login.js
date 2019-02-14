1ef379d96c309ee95fafe26f39c2a7547e5ab7c8
SirAbbott <tom@iynk.com>
2019-02-14 15:53:14 +0000

    sorted login error-handling

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -40,7 +40,7 @@ class Login extends React.Component {
       .catch(err => this.setState({ errors: err.response.data}))
     const data = { email: '', password: '' }
     this.setState({ data: data })
-
+    console.log(this.state.errors)
   }
 
   render() {
@@ -64,7 +64,7 @@ class Login extends React.Component {
                       value={this.state.data.email}
                       onChange={this.handleChange}
                     />
-                    {errors.email && <small>{errors.email}</small>}
+                    {errors.message && <small className="help is-danger">{errors.message}</small>}
                   </div>
                 </div>
                 <div className="field has-addons">
@@ -77,7 +77,7 @@ class Login extends React.Component {
                       value={this.state.data.password}
                       onChange={this.handleChange}
                     />
-                    {errors.password && <small>{errors.password}</small>}
+                    {errors.message && <small className="help is-danger">{errors.message}</small>}
                   </div>
                 </div>
                 <div className="control">
